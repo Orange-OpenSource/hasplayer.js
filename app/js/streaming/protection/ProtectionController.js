a882bbef01b81d92a869e362344fb734d2f90f21
Ingo Breuer <ibreuer@cocus.com>
2016-01-25 15:18:05 +0100

    fix: undefined messageType in onKeyMessage

--- app/js/streaming/protection/ProtectionController.js
+++ app/js/streaming/protection/ProtectionController.js
@@ -245,20 +245,21 @@ MediaPlayer.dependencies.ProtectionController = function() {
         onKeyMessage = function(e) {
             var self = this,
                 licenseMessage = null,
-                keyMessage;
+                keyMessage,
+                messageType;
 
             // Dispatch event to applications indicating we received a key message
-            keyMessage = e.data;
+            keyMessage = (e.data)? e.data : e;
+            messageType = (keyMessage.messageType) ? keyMessage.messageType : "license-request",
 
-            this.debug.log("[DRM] Key message: type = " + keyMessage.messageType);
+            this.debug.log("[DRM] Key message: type = " + messageType);
 
             this.eventBus.dispatchEvent({
                 type: MediaPlayer.dependencies.ProtectionController.events.KEY_MESSAGE,
                 data: keyMessage
             });
 
-            var messageType = (keyMessage.messageType) ? keyMessage.messageType : "license-request",
-                message = keyMessage.message,
+            var message = keyMessage.message,
                 sessionToken = keyMessage.sessionToken,
                 protData = getProtData(this.keySystem),
                 keySystemString = this.keySystem.systemString,
