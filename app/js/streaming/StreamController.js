58fa25d0381519b3ab16f5684bdd472d805862fb
unknown <OBBI7360@EB-100640407.ad.francetelecom.fr>
2016-05-17 18:33:24 +0200

    StreamController.js: add missing autoplay parameter in seek() function

--- app/js/streaming/StreamController.js
+++ app/js/streaming/StreamController.js
@@ -672,9 +672,9 @@ MediaPlayer.dependencies.StreamController = function() {
             }
         },
 
-        seek: function(time) {
+        seek: function(time, autoplay) {
             if (activeStream) {
-                activeStream.seek(time);
+                activeStream.seek(time, autoplay);
             }
         },
     };
