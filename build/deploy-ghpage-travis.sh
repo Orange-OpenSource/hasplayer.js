cbc9780b31a78270e4040cf918427fe596120e58
mbrechet <mbrechet.ext@orange.com>
2016-06-27 12:01:03 +0200

    [Travis] deploy build before executing fonctional tests

---
+++ build/deploy-ghpage-travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir -p out/dev/ 
+cd out/
+git init
+git config user.name "Travis-CI"
+git config user.email "mbrechet.ext@orange.com"
+cp  -r ../dist/ ./dev/
+git add .
+git commit -m "automatic deploy from Travis-CI"
+git push --force --quiet "https://github.com/Orange-OpenSource/hasplayer.js.git" master:gh-pages > /dev/null 2>&1
