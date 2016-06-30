d3ea25bce3bed2a3e5fb43f5835d0d7ef2c5df1d
mbrechet <mbrechet.ext@orange.com>
2016-06-30 17:41:41 +0200

    [Travis] update deployment script

--- build/deploy-ghpage-travis.sh
+++ build/deploy-ghpage-travis.sh
@@ -11,7 +11,7 @@ cd out/
 git init
 git config user.name "Travis-CI"
 git config user.email "mbrechet.ext@orange.com"
-cp  -r ../dist/ ./dev/
+cp  -r ../dist/* ./dev/
 git add .
 git commit -m "automatic deploy from Travis-CI"
 git push --force "git@github.com:Orange-OpenSource/hasplayer.js.git" master:gh-pages
