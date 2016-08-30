83f7c3fd75aa5491fbccc41e24c17eed5e450f16
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-08-30 11:27:08 +0200

    functional test: update test web applications

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,8 +1,8 @@
 define({
 
     DashIF: {
-        development: 'http://orange-opensource.github.io/hasplayer.js/dev/samples/DemoPlayer/index.html',
-        master: 'http://orange-opensource.github.io/hasplayer.js/1.3.1/samples/DemoPlayer/index.html',
+        development: 'http://orange-opensource.github.io/hasplayer.js/development/samples/DemoPlayer/index.html',
+        master: 'http://orange-opensource.github.io/hasplayer.js/1.4.0/samples/DemoPlayer/index.html',
         local:'http://localhost:8081/hasplayer.js-github/dist/samples/DemoPlayer/index.html'
     }
 
