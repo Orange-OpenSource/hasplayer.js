4f1fda975cda6031f5d270507b071b7305bbe067
Travis-CI <bertrand.berthelot@orange.com>
2017-04-06 08:10:46 +0000

    Automatic build from Travis-CI

--- development/hasplayer.js
+++ development/hasplayer.js
@@ -14,7 +14,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* Last build : 2017-3-30_15:10:56 / git revision : a778f44 */
+/* Last build : 2017-4-6_8:6:14 / git revision : 911dde3 */
 
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -59,6 +59,11 @@ var hasplayer = {},
  *
  */
 /*jshint -W020 */
+
+/**
+ * @class
+ * @classdesc MediaPlayer is the object used by the webapp to instanciante and control hasplayer.
+ */
 MediaPlayer = function () {
 
 //#region Private attributes/properties
@@ -66,8 +71,8 @@ MediaPlayer = function () {
     ////////////////////////////////////////// PRIVATE ////////////////////////////////////////////
     var VERSION_DASHJS = '1.2.0',
         VERSION = '1.10.0-dev',
-        GIT_TAG = 'a778f44',
-        BUILD_DATE = '2017-3-30_15:10:56',
+        GIT_TAG = '911dde3',
+        BUILD_DATE = '2017-4-6_8:6:14',
         context = new MediaPlayer.di.Context(), // default context
         system = new dijon.System(), // dijon system instance
         initialized = false,
@@ -1552,10 +1557,6 @@ MediaPlayer = function () {
     };
 };
 
-/**
- * @class
- * @classdesc MediaPlayer is the object used by the webapp to instanciante and control hasplayer.
- */
 MediaPlayer.prototype = {
     constructor: MediaPlayer
 };
