3696f3118abfc030be796e2bdfc01ec636877e78
Bertrand Berthelot <bertrand.berthelot@orange.com>
2017-10-19 14:13:46 +0200

    [FunctionalTests]: add simple webapp for testing

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,7 +1,4 @@
 define({
-    DashIF: {
-        development: 'http://orange-opensource.github.io/hasplayer.js/development/samples/DemoPlayer/index.html',
-        master: 'http://orange-opensource.github.io/hasplayer.js/latest/samples/DemoPlayer/index.html'
-    }
-
+    development: 'http://orange-opensource.github.io/hasplayer.js/development/samples/tests.html',
+    master: 'http://orange-opensource.github.io/hasplayer.js/latest/samples/tests.html'
 });
