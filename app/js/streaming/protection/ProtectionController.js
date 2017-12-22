5fb485d162aee3d36c8a02c20a12c46f9f5d5ec6
Bertrand Berthelot <bertrand.berthelot@orange.com>
2017-12-22 11:01:00 +0100

    Update ProtectionController.js
    Changed comment since in IE11 supports ArrayBuffer.isView

--- app/js/streaming/protection/ProtectionController.js
+++ app/js/streaming/protection/ProtectionController.js
@@ -43,7 +43,7 @@
  * each step manually (key system selection, session creation, session maintenance).
  */
 
-// Define ArrayBuffer.isView method in case it is not defined (like in IE11 for example)
+// Define ArrayBuffer.isView method in case it is not defined
 if (!ArrayBuffer.isView) {
     ArrayBuffer.isView = function(data) {
         return data && data.buffer instanceof ArrayBuffer;
