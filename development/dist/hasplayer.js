da02a7af2adb239179f39abfa3ba01b4110d8fda
Travis-CI <bertrand.berthelot@orange.com>
2018-02-06 14:14:43 +0000

    Automatic build from Travis-CI

--- development/dist/hasplayer.js
+++ development/dist/hasplayer.js
@@ -14,7 +14,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* Last build : 2018-2-6_13:52:38 / git revision : 79a6be3 */
+/* Last build : 2018-2-6_14:12:12 / git revision : b085641 */
 
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -71,8 +71,8 @@ MediaPlayer = function () {
     ////////////////////////////////////////// PRIVATE ////////////////////////////////////////////
     var VERSION_DASHJS = '1.2.0',
         VERSION = '1.14.0-dev',
-        GIT_TAG = '79a6be3',
-        BUILD_DATE = '2018-2-6_13:52:38',
+        GIT_TAG = 'b085641',
+        BUILD_DATE = '2018-2-6_14:12:12',
         context = new MediaPlayer.di.Context(), // default context
         system = new dijon.System(), // dijon system instance
         initialized = false,
@@ -28088,8 +28088,7 @@ Mss.dependencies.MssFragmentController = function() {
                 traf = null,
                 tfdt = null,
                 tfrf = null,
-                pos,
-                i = 0;
+                pos;
 
             // Create new fragment
             fragment = mp4lib.deserialize(bytes);
