24bef00186fa1d17e8ac037ca04321897c538d98
Travis-CI <bertrand.berthelot@orange.com>
2018-12-18 16:19:06 +0000

    Automatic build from Travis-CI

--- development/dist/hasplayer.js
+++ development/dist/hasplayer.js
@@ -14,7 +14,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* Last build : 2018-9-26_7:17:2 / git revision : a423ea3 */
+/* Last build : 2018-12-18_16:15:13 / git revision : d035103 */
 
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -71,8 +71,8 @@ MediaPlayer = function () {
     ////////////////////////////////////////// PRIVATE ////////////////////////////////////////////
     var VERSION_DASHJS = '1.2.0',
         VERSION = '1.16.0-dev',
-        GIT_TAG = 'a423ea3',
-        BUILD_DATE = '2018-9-26_7:17:2',
+        GIT_TAG = 'd035103',
+        BUILD_DATE = '2018-12-18_16:15:13',
         context = new MediaPlayer.di.Context(), // default context
         system = new dijon.System(), // dijon system instance
         initialized = false,
@@ -9224,7 +9224,7 @@ MediaPlayer.dependencies.Stream = function() {
             // Seeking at end of stream (= duration) does not work consistently across browsers and 'ended' event is then not always raised.
             // Then seek 2 sec. backward to enable 'ended' event to be raised.
             var backoffSeekToEnd = this.config.getParam("backoffSeekToEnd", "number", 2);
-            if (duration !== Infinity && time >= (duration - backoffSeekToEnd)) {
+            if (duration !== Infinity && time > (duration - backoffSeekToEnd)) {
                 setVideoModelCurrentTime.call(this, (duration - backoffSeekToEnd));
                 return;
             }
